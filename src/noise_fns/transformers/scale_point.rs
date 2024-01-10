13556d488fe4573c15a0017103fdcf2902f44326
v0x0g <v0x0g-git.swm3u@simplelogin.com>
2024-01-10 18:02:32 +1000

    Add `Debug` derive impl for `noise_fns::transformers::*`

--- src/noise_fns/transformers/scale_point.rs
+++ src/noise_fns/transformers/scale_point.rs
@@ -5,7 +5,7 @@ use crate::noise_fns::NoiseFn;
 ///
 /// The get() method multiplies the coordinates of the input value with a
 /// scaling factor before returning the output value from the source function.
-#[derive(Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct ScalePoint<Source> {
     /// Source function that outputs a value
     pub source: Source,
--- src/noise_fns/transformers/translate_point.rs
+++ src/noise_fns/transformers/translate_point.rs
@@ -5,7 +5,7 @@ use crate::noise_fns::NoiseFn;
 ///
 /// The get() method moves the coordinates of the input value by a translation
 /// amount before returning the output value from the source function.
-#[derive(Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct TranslatePoint<Source> {
     /// Source function that outputs a value
     pub source: Source,
